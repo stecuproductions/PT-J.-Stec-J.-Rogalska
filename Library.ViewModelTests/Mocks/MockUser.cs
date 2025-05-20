a5d2a01539b4dde5411434d011044a56f4c6b7b3
Julia <250353@edu.p.lodz.pl>
2025-05-20 22:29:25 +0200

    a

---
+++ Library.ViewModelTests/Mocks/MockUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Logic.API;
+
+namespace Library.ViewModelTests.Mocks
+{
+    internal class MockUser : IUserLogic
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
---
+++ Library.ViewModelTests/ReturnViewModelTests.cs
@@ -0,0 +1,29 @@
+﻿using Library.Logic.API;
+using Library.ViewModelTests.Mocks;
+
+namespace Library.ReturnViewModelTests
+{
+    [TestClass]
+    public class ReturnViewModelTests
+    {
+        private Guid _testUserId = Guid.NewGuid();
+        private MockLibraryService _mockService;
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockService = new MockLibraryService();
+            _mockService.AddBookLogic("Test 1", "Author A");
+            _mockService.AddBookLogic("Test 2", "Author B");
+            IBookLogic book1 = _mockService.GetNBooksLogic(1, 0).First();
+            IBookLogic book2 = _mockService.GetNBooksLogic(1, 1).First();
+            _mockService.BorrowBookLogic(_testUserId, book1.Id);
+            _mockService.BorrowBookLogic(_testUserId, book2.Id);
+        }
+        [TestMethod]
+        public void LoadBorrowedBooks()
+        {
+            MockUser user = new MockUser { Id = _testUserId};
+
+        }
+    }
+}
